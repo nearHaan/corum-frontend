8a47e5a25106b5646a983ed8d24b1664a3c1cf68
nearHaan <farhaan.nizam675@gmail.com>
2025-09-21 20:01:20 +0530

    add sidebar

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,7 +4,7 @@ import { Button } from "../../components/Button";
 export default function HomePage() {
   return (
     <div className="min-h-screen w-screen overflow-x-hidden flex flex-col items-center justify-center font-mono text-black">
-      <header className="w-full flex items-center justify-between shadow-[0px_2px_#00000020] z-30 px-5">
+      <header className="relative sticky top-0 w-full flex items-center justify-between shadow-[0px_2px_#00000020] z-30 px-5">
         <h2 className="text-2xl my-2">CORUM</h2>
         <div className="flex-1 max-w-md rounded-lg my-2 mx-5 ring-1 ring-[#00000020]">
           <input
@@ -21,7 +21,8 @@ export default function HomePage() {
           <div className="w-10 h-10 rounded-full bg-gray-400"></div>
         </div>
       </header>
-      <main className="flex-1">
+      <main className="w-full h-full flex items-center justify-center">
+        <div className="w-80 h-screen bg-red-300">//</div>
         <Outlet />
       </main>
     </div>
