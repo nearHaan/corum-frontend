8ed13952b6b6ce81e5f4b3badc1b5d47b0e96fae
nearHaan <farhaan.nizam675@gmail.com>
2025-10-17 00:59:56 +0530

    ui update

--- src/pages/Home/tabs/Questions.tsx
+++ src/pages/Home/tabs/Questions.tsx
@@ -1,48 +1,74 @@
+import { useState } from "react";
+import { QuestionBox } from "../../../components/QuestionBox";
+import { ForumElement } from "../../../components/ForumElem";
+
 export default function QuestionsPage() {
-  const quickTags = ["node.js", "python"];
+  const [questions, setQuestions] = useState([
+    {
+      title: "dfdf dfdf dfdfd fd f df df d fdfdfdfd fdfdfdf",
+      votes: 12,
+      tags: ["node.js", "mongodb"],
+      views: 250,
+      comments: 10,
+    },
+    {
+      title: "dfdf dfdfd fdfdfdf dfdfdfdf df",
+      votes: 8,
+      tags: ["react", "hooks"],
+      views: 180,
+      comments: 5,
+    },
+  ]);
+  const [forums, setForums] = useState([
+    {
+      id: "sdsd",
+      votes: 12,
+      desc: "dsddfdf df dfdf dffd f df dffd",
+      comments: [
+        {
+          id: "ddffd",
+          username: "fdfd",
+          comment: "dfdfd",
+          timestamp: "dfdf",
+        },
+      ],
+    },
+    {
+      id: "fdfddfdf",
+      votes: 13,
+      desc: "dsddfdf df dfdf dffd f df dffd",
+      comments: [
+        {
+          id: "dfdfdfddfd",
+          username: "fdfd",
+          comment: "dfdfd",
+          timestamp: "dfdf",
+        },
+      ],
+    },
+  ]);
   return (
     <div className="w-full h-full flex flex-col p-2 items-center justify-center">
       <div className="h-10"></div>
-      <div className="w-full ring-1 ring-[#00000020] mb-2 rounded-xl flex items-center gap-x-2">
-        <div className="felx items-center justify-center flex-col py-10 px-8">
-          <p className="text-4xl">12</p>
-          <p className="text-xs w-full text-center">Votes</p>
-        </div>
-        <div>
-          <h2 className="text-2xl mb-5">Title Here</h2>
-          <div className="grid mb-5 grid-cols-2 gap-2 w-fit">
-            {quickTags.map((item) => (
-              <p className="text-gray-500 mx-1 text-sm rounded-full py-1 px-2 bg-gray-100 w-fit">
-                {item}
-              </p>
-            ))}
-          </div>
-          <div className="flex gap-x-5">
-            <p className="text-sm text-gray-400">200 views</p>
-            <p className="text-sm text-gray-400">200 comments</p>
-          </div>
-        </div>
-      </div>
-      <div className="w-full ring-1 ring-[#00000020] mb-2 rounded-xl flex items-center gap-x-2">
-        <div className="felx items-center justify-center flex-col py-10 px-8">
-          <p className="text-4xl">12</p>
-          <p className="text-xs w-full text-center">Votes</p>
-        </div>
-        <div>
-          <h2 className="text-2xl mb-5">Title Here</h2>
-          <div className="grid mb-5 grid-cols-2 gap-2 w-fit">
-            {quickTags.map((item) => (
-              <p className="text-gray-500 mx-1 text-sm rounded-full py-1 px-2 bg-gray-100 w-fit">
-                {item}
-              </p>
-            ))}
-          </div>
-          <div className="flex gap-x-5">
-            <p className="text-sm text-gray-400">200 views</p>
-            <p className="text-sm text-gray-400">200 comments</p>
-          </div>
-        </div>
-      </div>
+      {questions.map((question, index) => (
+        <QuestionBox
+          key={index}
+          title={question.title}
+          votes={question.votes}
+          tags={question.tags}
+          views={question.views}
+          comments={question.comments}
+        />
+      ))}
+      {forums.map((forum, index) => (
+        <ForumElement
+          key={index}
+          id={forum.id}
+          votes={forum.votes}
+          desc={forum.desc}
+          comments={forum.comments}
+        />
+      ))}
     </div>
   );
 }
