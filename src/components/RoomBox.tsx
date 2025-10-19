24317b9b611ca053b4849f5e1e688556493b5d8e
nearHaan <farhaan.nizam675@gmail.com>
2025-10-19 20:11:42 +0530

    add onClick

--- src/components/RoomBox.tsx
+++ src/components/RoomBox.tsx
@@ -1,11 +1,15 @@
 type RoomProps = {
   id: string;
   name: string;
+  onClick: Function;
 };
 
-export const RoomBox: React.FC<RoomProps> = ({ id, name }) => {
+export const RoomBox: React.FC<RoomProps> = ({ id, name, onClick }) => {
   return (
-    <div className="h-45 w-45 bg-neutral-100 rounded-2xl flex items-center justify-center">
+    <div
+      onClick={() => onClick(id)}
+      className="cursor-pointer h-45 w-45 bg-neutral-100 rounded-2xl flex items-center justify-center"
+    >
       <p className="text-black text-lg">{name}</p>
     </div>
   );
